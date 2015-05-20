df73f62f463ca99e262c9d01af84184c0a79e9db
Lily Bader <baderlily@gmail.com>
2015-05-20 15:19:32 -0700

    Update challenge.js

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,41 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function (fav, guess) {
+	if (guess > fav) {
+		return "Too high";
+	}
+	else if (guess < fav) {
+		return "Too low";
+	} 
+	else {
+		return "You got it!"
+	}
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function (w, x, y, z) {
+	if ((w == 3 || w == 5 || w == 7) && (x == 2) && (y >= 5 || y <= 100) && (z < 9 || z < 20)) {
+		return "correct";
+	} 
+	else {
+		return "incorrect";
+	}
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function (item, money) {
+	if ((item == "MacBook Air") && (money >= 999)) {
+		return true;
+	} 
+	else if 
+		((item == "MacBook Pro") && (money >= 1299)) {
+			return true;
+	} 
+	else if 
+		((item == "Mac Pro") && (money >= 2499)) {
+				return true;
+	}
+	else if 
+		((item == "Apple Sticker") && (money >= 1)) {
+			return true;
+	}
+	else {
+		return false;
+	}
+};
