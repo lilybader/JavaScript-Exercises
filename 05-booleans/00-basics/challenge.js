61c7f5587a5897a0f5913b34de7f7da2d7d52eae
lilybader <vagrant@precise32.(none)>
2015-05-20 21:31:22 +0000

    solved boolean basics

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,35 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+	return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+	if (one === two) {
+		return false;
+	} else {
+		return true;
+	}
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	if ((lower < middle) && (middle < upper)) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 module.exports.outsideRanges = function(number) {
-
+	if ((number < 10 || number > 20) && (number <= 42 || number > 75) && (number <= 1 || number >= 6)) {
+		return true;
+	} else { 
+		return false; 
+	}
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+	if ((name == "NYTimes" || name == "LATimes") && (price >= 1)) {
+		return true;
+	} else {
+		return false;
+	}
 };
