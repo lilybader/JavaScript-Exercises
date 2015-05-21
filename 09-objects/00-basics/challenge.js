d668f75d90553327ab7bc00591b407111206597a
Lily Bader <baderlily@gmail.com>
2015-05-20 17:23:17 -0700

    Update challenge.js

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,33 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {};
+  course.title = courseTitle;
+  course.duration = courseDuration;
+  course.students = courseStudents;
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object.hasOwnProperty(newProp) == true) {
+    return object;
+  } else {
+    object[newProp] = newValue;
+    return object;
+  };
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function(item, price) {
+  var priceTable = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  };
+
+
+  return price >= priceTable[item];
+};
